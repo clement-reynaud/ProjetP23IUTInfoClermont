b837071aa237ad9c726e1a87b95a1899c2d92120
Mael-Petitcolin <74175850+Mael-Petitcolin@users.noreply.github.com>
2021-01-25 11:59:50 +0100

    Update client.h

--- Code/ode/projetODE/projetODE/client.h
+++ Code/ode/projetODE/projetODE/client.h
@@ -6,4 +6,4 @@
 #include "creationBuggy.h"
 
 void initilizeBuggyDimentions(Buggy* buggies, dReal Ilength, dReal Iwidth, dReal Iheight, float Iradius, float IcylinderLenght, float IcylinderRadius, float IboxLenght, float IboxWidth, float IboxHeight, float IsphereRadius);
-void drawBuggyWithPos(const dReal** positions, Buggy buggies);
+void drawBuggyWithPos(const dReal** positions, int* colors, int nbBulletMax);
